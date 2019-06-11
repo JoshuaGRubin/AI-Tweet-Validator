eec0c891bac2e576460a32f0864e87164dd7b6e7
Joshua Rubin <jgrubin@gmail.com>
2019-06-10 21:55:24 -0700

    Updated downloadTweetsByUser so that it could also be imported in Python (in addition to being called from command line).

--- source/data_mining/downloadTweetsByUser.py
+++ source/data_mining/downloadTweetsByUser.py
@@ -5,7 +5,7 @@ Gathers the tweet history of any twitter user.
 Requires valid Twitter API credentials.
 See: http://develope.twitter.com
 
-Intended to be called from the command line or Bash script.
+Can be imported by Python or called from the command line or Bash script
 
   > ./downloadTweetsByUser.py --help
 
@@ -64,44 +64,47 @@ def getTweetsByUser(user, outputPath = None, maxTweets = 10, verbose = True):
         print('Retrieved',len(tweets), 'tweets for',
               user + '. Wrote to', outputPath + '.')
 
-######
-# Create command line argument parser, populate, and parse
-parser = argparse.ArgumentParser(description=
-             "Retrieve the tweets of a Twitter user and write to a json file."
-             "  TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, "
-             "TWITTER_ACCESS_TOKEN, and TWITTER_ACCESS_SECRET must be valid "
-             "environment variables containing Twitter API credentials."
-             "Requires tweepy in PYTHONPATH. ")
+# If I'm being run as a script... otherwise just export getTweetsByUser. 
+if __name__ == '__main__':
 
-parser.add_argument('TwitterHandle', type=str,
-                    help="The user whose tweets you'd like to retrieve.")
-
-parser.add_argument('-p','--outputDirectory', type=str, default = '.',
-                     help="Where to write output json if not PWD.")
-
-parser.add_argument('-n','--numMaxTweets', type=str, default = '10',
-                     help="What's the largest number of tweets to retriecve?")
-
-parser.add_argument('-v', '--verbose', action="count", default=1,
-                     help="Suppress status messages.")
-
-args = parser.parse_args()
-#####
-
-if args.verbose:
-    print('Getting (max: '+args.numMaxTweets+') tweets from '
-           + args.TwitterHandle+'.')
-
-if os.path.isdir(args.outputDirectory):
-    if args.verbose:
-        print('Path '+ args.outputDirectory +' exists.')
-else:
-    if args.verbose:
-        print("Path doesn't exist; creating"+args.outputDirectory+".")
-    os.makedirs(args.outputDirectory)
-
-      
-getTweetsByUser('@StephenAtHome',
-                maxTweets = int(args.numMaxTweets),
-                outputPath = args.outputDirectory,
-                verbose = args.verbose )
+        ######
+        # Create command line argument parser, populate, and parse
+        parser = argparse.ArgumentParser(description=
+                     "Retrieve the tweets of a Twitter user and write to a json file."
+                     "  TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, "
+                     "TWITTER_ACCESS_TOKEN, and TWITTER_ACCESS_SECRET must be valid "
+                     "environment variables containing Twitter API credentials."
+                     "Requires tweepy in PYTHONPATH. ")
+        
+        parser.add_argument('TwitterHandle', type=str,
+                            help="The user whose tweets you'd like to retrieve.")
+        
+        parser.add_argument('-p','--outputDirectory', type=str, default = '.',
+                             help="Where to write output json if not PWD.")
+        
+        parser.add_argument('-n','--numMaxTweets', type=str, default = '10',
+                             help="What's the largest number of tweets to retriecve?")
+        
+        parser.add_argument('-v', '--verbose', action="count", default=1,
+                             help="Suppress status messages.")
+        
+        args = parser.parse_args()
+        #####
+        
+        if args.verbose:
+            print('Getting (max: '+args.numMaxTweets+') tweets from '
+                   + args.TwitterHandle+'.')
+        
+        if os.path.isdir(args.outputDirectory):
+            if args.verbose:
+                print('Path '+ args.outputDirectory +' exists.')
+        else:
+            if args.verbose:
+                print("Path doesn't exist; creating"+args.outputDirectory+".")
+            os.makedirs(args.outputDirectory)
+        
+              
+        getTweetsByUser('@StephenAtHome',
+                        maxTweets = int(args.numMaxTweets),
+                        outputPath = args.outputDirectory,
+                        verbose = args.verbose )
