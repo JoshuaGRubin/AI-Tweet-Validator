2956fbf96d9db0024da8e0325796a9aae1f84010
Joshua Rubin <jgrubin@gmail.com>
2019-06-17 13:13:48 -0700

    Encapsulated embedding model in a class and set it up so that the slow initialization step is only incurred once. Also updated variable names and spacing for style purposes.

--- source/data_mining/embedTweets.py
+++ source/data_mining/embedTweets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+""" 
 Reads files of [[tweet,date]] Applies Universal Sentence Encoder to to the
 tweet to produce a third column with a 512 component embedding.  Writes a json
 output file containing this three column data.
@@ -16,48 +16,59 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 # Retrieve Universal Eentence Sncoder.
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
-embed = hub.Module(module_url)
+MODULE_URL = "https://tfhub.dev/google/universal-sentence-encoder/2"
 
-def embedTweets(inputDirectory, outputDirectory): 
+class SentenceEncoder:
+    def __init__(self, url = MODULE_URL):
+        self.session = tf.Session()
+        self.embed = hub.Module(url)
+        self.session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        self.messagesPlaceholder = tf.placeholder(dtype=tf.string, shape=[None])
+        self.output = self.embed(self.messagesPlaceholder)
+    
+    def embed_phrases(self, phrase_list):
+        return self.session.run(self.output, feed_dict={self.messagesPlaceholder: phrase_list})
+    
+    def __del__(self):
+        self.session.close()
+        
+def embed_tweets_from_directories(input_directory_path, output_directory_path): 
+    
+    # Initialize embedding model.
+    print('Initializing embedding model.  May take a few seconds.')
+    sentence_encoder = SentenceEncoder()
     
     # Grab the files from the input directory
-    tweetFiles = [f for f in os.listdir(inputDirectory) if f[0]=='@']
+    tweet_files = [f for f in os.listdir(input_directory_path) if f[0]=='@']
         
-    for fileName in tweetFiles:   
+    for file_name in tweet_files:   
             
-        print('Loading ' + fileName + '.', end=' ')
-        
-        inputPath  = os.path.join(inputDirectory, fileName)
-
-        with open(inputPath, 'r') as file:
-            inData =  json.loads(file.read())
+        print('Loading ' + file_name + '.', end=' ')        
+        input_file_path  = os.path.join(input_directory_path, file_name)
+        with open(input_file_path, 'r') as file:
+            in_data =  json.loads(file.read())
     
-        data = pd.DataFrame(inData, columns=['tweet','date'])
-    
-        print(len(data) + ' tweets.')
-    
-        print('Building embedding model (may take time) and running inference.')
+        data = pd.DataFrame(in_data, columns=['tweet','date'])    
+        print('\t' + str(len(data)) + ' tweets loaded.')
                
+        tweets = data['tweet'].values
+        
         # produce a matched length vector based o the 'tweet' column of 'data' 
-        with tf.Session() as session:
-          session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-          message_embeddings = session.run(embed(data['tweet'].values))
+        message_embeddings = sentence_encoder.embed_phrases(tweets)
                 
         # add the embeddings to the dataframe  
         data['embeddings'] = message_embeddings.tolist()
         
-        print(str(len(data)), 'tweets processed.')
+        print('\t' + str(len(data)), 'tweets processed.')
             
-        outputPath = os.path.join(outputDirectory, fileName)    
-    
-        with open(outputPath, 'w') as file:
+        output_path = os.path.join(output_directory_path, file_name)    
+        with open(output_path, 'w') as file:
             file.write(data.to_json(orient='values')) 
         
 # If I'm being run as a script... otherwise just provide getTweetsByUser. 
 if __name__ == '__main__':
     
-    inputDirectory  = '../../data/preprocessed'
-    outputDirectory = '../../data/processed'
+    input_directory  = '../../data/preprocessed'
+    output_directory = '../../data/processed'
     
-    embedTweets(inputDirectory, outputDirectory)
+    embed_tweets_from_directories(input_directory, output_directory)
