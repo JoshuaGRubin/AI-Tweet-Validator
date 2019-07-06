7706f31b7b9f0b12d50283d0d649b5e7ab0586b7
Joshua Rubin <jgrubin@gmail.com>
2019-07-05 17:14:51 -0700

    Made tweetvalidator (our core library) an installable package.  Added it (and Pandas) to requirements.txt.

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="tweetvalidator",
+    packages=["tweetvalidator"],
+    version="0.0.1",
+    author="Joshua Rubin",
+    author_email="jgrubin@gmail.com",
+    description="Characterizes twitter users' activity and uses that to flag out-of-character content.",
+)
