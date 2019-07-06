025b79466f41d2464a4f655fce3e1f45f143af18
Joshua Rubin <jgrubin@gmail.com>
2019-07-05 17:10:32 -0700

    Added a place for those installing with conda to insert their credentials.

---
+++ build/twitter_creds.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Consume:
+export TWITTER_CONSUMER_KEY='XXX'
+export TWITTER_CONSUMER_SECRET='XXX'
+
+# Access:
+export TWITTER_ACCESS_TOKEN='XXX'
+export TWITTER_ACCESS_SECRET='XXX'
